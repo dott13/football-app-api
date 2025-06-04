a9e160408b5de8522510843f30a564badf44e271
dott13 <supreme.notfound@gmail.com>
2025-06-04 16:19:53 +0300

    added packages and scaffolded the proejct structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer};
+
+mod handlers;
+mod scrapers;
+mod models;
+mod utils;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+
+    HttpServer::new(|| App::new())
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
