d96f856b6c3ab2ff11fef03b9f60208dc95136c5
dott13 <supreme.notfound@gmail.com>
2025-07-07 23:11:40 +0300

    made a table parser for any table so no repetition of code

---
+++ src/scrapers/helpers/table_parser.rs
@@ -0,0 +1,77 @@
+use std::collections::HashMap;
+
+use scraper::{ElementRef, Html, Selector};
+
+pub fn parse_table<T, F>(
+    html: &str,
+    table_selector: &str,
+    header_selector: &str,
+    row_selector: &str,
+    map_row: F
+) -> Vec<T>
+where 
+    F: Fn(&HashMap<String, scraper::ElementRef>) -> T 
+{
+    let document = Html::parse_document(html);
+
+    let table = document.
+        select(&Selector::parse(table_selector).unwrap())
+        .next()
+        .expect("No table found");  
+
+    let header = Selector::parse(header_selector).expect("No header found");
+
+    let row_sel = Selector::parse(row_selector).expect("Invalid Selector");
+
+    let mut headers = Vec::new();
+    for th in table.select(&header) {
+        let label = th
+            .text()
+            .collect::<String>()
+            .trim()
+            .to_string();
+        //filter based on the label so we get only the clean ones we need
+        if label.eq_ignore_ascii_case("name") || label == "#" || label.is_empty() {
+            continue;
+        }
+        //Using the colspan of the table headers we actually get the true number of labels in the headers we need
+        let span = th
+            .value()
+            .attr("colspan")
+            .and_then(|s| s.parse::<usize>().ok())
+            .unwrap_or(1);
+        //push the labels to the headers vector
+        for _ in 0..span {
+            headers.push(label.clone());
+        }
+    }
+
+    let cells_sel = Selector::parse("td").unwrap();
+    let mut results: Vec<T> = Vec::new();
+    for row in table.select(&row_sel) {
+
+        let cells: Vec<ElementRef> = row.select(&cells_sel).collect();
+        let mut row_map = HashMap::new();
+        for (i, hdr) in headers.iter().enumerate() {
+            row_map.insert(hdr.clone(), cells[i].clone());
+        }
+
+        results.push(map_row(&row_map));
+    }
+
+    results
+}
+
+pub fn parse_table_default<T, F>(
+    html: &str,
+    map_row: F
+) -> Vec<T>
+where F: Fn(&HashMap<String, ElementRef>) -> T {
+    parse_table(
+        html,
+        "table.items",
+        "thead tr th",
+        "tbody tr",
+        map_row
+    )
+}
